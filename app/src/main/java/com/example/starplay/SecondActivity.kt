7b07ad114fc82500c548356a534d63ec5fe02920
Prajjwal2022 <prajjwaldubey1998@gmail.com>
2022-11-18 13:13:17 +0530

    Second Commit

--- app/src/main/java/com/example/starplay/SecondActivity.kt
+++ app/src/main/java/com/example/starplay/SecondActivity.kt
@@ -1,12 +1,14 @@
 package com.example.starplay
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SecondActivity : AppCompatActivity() {
     lateinit var entered_name:TextView
@@ -14,6 +16,9 @@ class SecondActivity : AppCompatActivity() {
     lateinit var translate:Button
     lateinit var fade:Button
     lateinit var star:ImageView
+    lateinit var bg_color:Button
+    lateinit var constraint_layout:ConstraintLayout
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -21,10 +26,16 @@ class SecondActivity : AppCompatActivity() {
         translate=findViewById(R.id.translate)
         fade=findViewById(R.id.fade)
         star=findViewById(R.id.star_image)
+        bg_color=findViewById(R.id.bg_color)
+        constraint_layout=findViewById(R.id.constraint_layout)
 
         entered_name=findViewById(R.id.entered_name)
         val user_name=intent.getStringExtra("UserName")
         entered_name.text=" Welcome !! $user_name"
+
+        bg_color.setOnClickListener {
+           constraint_layout.setBackgroundColor(R.color.warm)
+        }
 
         fade.setOnClickListener {
             fader()
